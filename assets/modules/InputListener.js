0a6d2ea1def325aa3c1e49b98c65a3905e129c6b
Deshan Nawanjana <deshan.uok@gmail.com>
2024-11-26 13:47:03 +0530

    Module Update

---
+++ assets/modules/InputListener.js
@@ -0,0 +1,54 @@
+export const InputListener = class {
+
+  constructor() {
+    this.enabled = true
+    this.activeKeys = {}
+    this.events = []
+    // add event listeners
+    this._addActiveKeyListeners()
+    this._addEventListeners()
+  }
+
+  addEvent(conditions, callback) {
+    this.events.push({ conditions: conditions, callback })
+  }
+
+  _addActiveKeyListeners() {
+    // active key listeners
+    window.addEventListener('keydown', e => {
+      if (e.key === 'Shift' || e.key === 'Alt' || e.key === 'Control') {
+        this.activeKeys[e.key] = true
+      } else { this.activeKeys[e.key.toUpperCase()] = true }
+    })
+    // keyup listener
+    window.addEventListener('keyup', e => {
+      if (e.key === 'Shift' || e.key === 'Alt' || e.key === 'Control') {
+        delete this.activeKeys[e.key]
+      } else { delete this.activeKeys[e.key.toUpperCase()] }
+    })
+  }
+
+  _addEventListeners() {
+    // mouse events
+    window.addEventListener('mousedown', event => this._checkListener(event))
+    window.addEventListener('mouseup', event => this._checkListener(event))
+    window.addEventListener('click', event => this._checkListener(event))
+    window.addEventListener('mousemove', event => this._checkListener(event))
+    // key events
+    window.addEventListener('keydown', event => this._checkListener(event))
+    window.addEventListener('keyup', event => this._checkListener(event))
+    window.addEventListener('keypress', event => this._checkListener(event))
+  }
+
+  _checkListener(event) {
+    this.events.forEach(obj => {
+      // match listener with event
+      const flag = Object.keys(obj.conditions).every(key => {
+        return event[key] === obj.conditions[key]
+      })
+      // callback
+      if (flag) { obj.callback({ event: event, data: obj }) }
+    })
+  }
+
+}
--- index.js
+++ index.js
@@ -1,172 +1,183 @@
-const width = window.innerWidth
-const height = window.innerHeight
+import { CharacterControls } from "./assets/modules/CharacterControls.js"
+import { InputListener } from "./assets/modules/InputListener.js"
 
 // model selector
-const select = document.querySelector('select')
+const select = document.querySelector("select")
 // get current model
-const model = location.search.includes('model=')
-    ? location.search.split('model=')[1].split('&')[0]
-    : 'character_1'
+const model = new URLSearchParams(window.location.search)
+  .get("model") || "character_1"
 // set model for selector
 select.value = model
-// on select
-select.addEventListener('input', () => {
-    // set model on url
-    location = '?model=' + select.value
+// switch character model
+select.addEventListener("input", () => {
+  window.location.href = "?model=" + select.value
 })
 
 // three modules
 const scene = new THREE.Scene()
-const renderer = new THREE.WebGLRenderer({ antialias : true })
-const camera = new THREE.PerspectiveCamera(45, width / height, 0.2, 500)
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+const camera = new THREE.PerspectiveCamera(45, 0.6, 0.2, 500)
 
 // setup modules
-scene.background = new THREE.Color('#333')
-document.body.appendChild(renderer.domElement)
+scene.background = new THREE.Color("#333")
 camera.position.set(0, 0.7, -1)
 camera.rotation.set(0.1, Math.PI, 0)
 
 // ground setup
 const grounds = []
-const mat_1 = new THREE.MeshBasicMaterial({ color : 'orange' })
-const mat_2 = new THREE.MeshBasicMaterial({ color : 'white' })
-for(let x = -4; x < 4; x += 1) {
-    for(let y = -4; y < 4; y += 1) {
-        const a = Math.abs(x % 2)
-        const b = Math.abs(y % 2)
-        const geo = new THREE.PlaneGeometry()
-        const mat = a ? b ? mat_1 : mat_2 : b ? mat_2 : mat_1 
-        const box = new THREE.Mesh(geo, mat)
-        box.position.set(x, 0, y)
-        box.rotation.set(-Math.PI / 2, 0, 0)
-        scene.add(box)
-        grounds.push(box)
-    }
+const mat_1 = new THREE.MeshBasicMaterial({ color: "orange" })
+const mat_2 = new THREE.MeshBasicMaterial({ color: "white" })
+for (let x = -4; x < 4; x += 1) {
+  for (let y = -4; y < 4; y += 1) {
+    const a = Math.abs(x % 2)
+    const b = Math.abs(y % 2)
+    const geo = new THREE.PlaneGeometry()
+    const mat = a ? b ? mat_1 : mat_2 : b ? mat_2 : mat_1
+    const box = new THREE.Mesh(geo, mat)
+    box.position.set(x, 0, y)
+    box.rotation.set(-Math.PI / 2, 0, 0)
+    scene.add(box)
+    grounds.push(box)
+  }
 }
 
 const unlitModel = obj => {
-    obj.traverse((child) => {
-        if(child.isMesh) {
-            // missing maps
-            child.material.emissive = child.material.color
-            child.material.emissiveMap = child.material.map
-            // to mesh basic
-            var prevMaterial = child.material
-            child.material = new THREE.MeshBasicMaterial()
-            THREE.MeshBasicMaterial.prototype.copy.call(child.material, prevMaterial)
-        }
-    })
-    return obj
+  obj.traverse((child) => {
+    if (child.isMesh) {
+      // missing maps
+      child.material.emissive = child.material.color
+      child.material.emissiveMap = child.material.map
+      // to mesh basic
+      var prevMaterial = child.material
+      child.material = new THREE.MeshBasicMaterial()
+      THREE.MeshBasicMaterial.prototype.copy.call(child.material, prevMaterial)
+    }
+  })
+  return obj
 }
 
+// get character model source path
+const path = `./assets/characters/${model}/scene.fbx`
+
 // load model
-new THREE.FBXLoader().load('./assets/'+ model +'/scene.fbx', model => {
-    // input listener
-    const listener = new InputListener()
-    // rescle character
-    model.scale.set(0.37, 0.37, 0.37)
-    // unlit character
-    unlitModel(model)
-    // create character
-    const character = new CharacterControls(model)
-    // setup character
-    character.setModules(scene, camera, renderer)
-    character.groundObjects = grounds
-    character.listener = listener
-    character.setDimensions(0.342, 0.375, 0.165)
+new THREE.FBXLoader().load(path, model => {
 
-    // animation map
-    character.setAnimationMap({
-        'ani_idle' : { speed : 1, name : 'anm_01000004' },
-        'ani_walk' : { speed : 1, name : 'anm_01020001' },
-        'ani_fist' : { speed : 1, name : 'anm_00020001_s', start : 10, end : 25 },
-        'ani_kick' : { speed : 1, name : 'anm_00020001_s', start : 20, end : 30 },
-        'ani_run'  : { speed : 1, name : 'anm_01020002' }
-    })
+  // input listener
+  const listener = new InputListener()
 
-    // play idle as default
-    character.play('ani_idle', false)
+  // rescle character
+  model.scale.set(0.37, 0.37, 0.37)
 
-    // character speeds
-    let walk_speed = 0.007
-    let run_speed = 0.03
+  // unlit character
+  unlitModel(model)
 
-    // method to slow
-    const slow = () => {
-        walk_speed = 0.003
-        run_speed = 0.007
+  // create character
+  const character = new CharacterControls(model)
+
+  // setup character
+  character.setModules(scene, camera, renderer)
+  character.groundObjects = grounds
+  character.listener = listener
+  character.setDimensions(0.342, 0.375, 0.165)
+
+  // animation map
+  character.setAnimationMap({
+    "ani_idle": { speed: 1, name: "anm_01000004" },
+    "ani_walk": { speed: 1, name: "anm_01020001" },
+    "ani_fist": { speed: 1, name: "anm_00020001_s", start: 10, end: 25 },
+    "ani_kick": { speed: 1, name: "anm_00020001_s", start: 20, end: 30 },
+    "ani_run": { speed: 1, name: "anm_01020002" }
+  })
+
+  // play idle as default
+  character.play("ani_idle", false)
+
+  // character speeds
+  let walk_speed = 0.007
+  let run_speed = 0.03
+
+  // method to slow
+  const slow = () => {
+    walk_speed = 0.003
+    run_speed = 0.007
+  }
+
+  // method to fast
+  const fast = () => {
+    walk_speed = 0.007
+    run_speed = 0.03
+  }
+
+  // action map
+  character.setActionMap({
+    "idle": {
+      animation: { id: "ani_idle" },
+      inspector: keys => !keys.W && !keys.A && !keys.S && !keys.D
+    },
+    "walk": {
+      animation: { id: "ani_walk" },
+      inspector: keys => (keys.W || keys.A || keys.S || keys.D) && !keys.Shift,
+      callback: obj => {
+        character.move(obj.keys.W, obj.keys.A, obj.keys.S, obj.keys.D, walk_speed)
+      }
+    },
+    "run": {
+      animation: { id: "ani_run" },
+      inspector: keys => (keys.W || keys.A || keys.S || keys.D) && keys.Shift,
+      callback: obj => {
+        character.move(obj.keys.W, obj.keys.A, obj.keys.S, obj.keys.D, run_speed)
+      }
+    },
+    "fist": {
+      animation: { id: "ani_fist", animationLock: true, onStart: slow, onEnd: fast },
+      executor: { type: "click", button: 0 }
+    },
+    "kick": {
+      animation: { id: "ani_kick", animationLock: true, onStart: slow, onEnd: fast },
+      executor: { type: "click", button: 2 }
     }
+  })
 
-    // method to fast
-    const fast = () => {
-        walk_speed = 0.007
-        run_speed = 0.03
-    }
+  // enable character
+  character.enable()
 
-    // action map
-    character.setActionMap({
-        'idle' : {
-            animation : { id : 'ani_idle' },
-            inspector : keys => !keys.W && !keys.A && !keys.S && !keys.D
-        },
-        'walk' : {
-            animation : { id : 'ani_walk' },
-            inspector : keys => (keys.W || keys.A || keys.S || keys.D) && !keys.Shift,
-            callback : obj => {
-                character.move(obj.keys.W, obj.keys.A, obj.keys.S, obj.keys.D, walk_speed)
-            }
-        },
-        'run' : {
-            animation : { id : 'ani_run' },
-            inspector : keys => (keys.W || keys.A || keys.S || keys.D) && keys.Shift,
-            callback : obj => {
-                character.move(obj.keys.W, obj.keys.A, obj.keys.S, obj.keys.D, run_speed)
-            }
-        },
-        'fist' : {
-            animation : { id : 'ani_fist', animationLock : true, onStart : slow, onEnd : fast },
-            executor : { type : 'click', button : 0 }
-        },
-        'kick' : {
-            animation : { id : 'ani_kick', animationLock : true, onStart : slow, onEnd : fast },
-            executor : { type : 'click', button : 2 }
-        }
-    })
+  // render method
+  const render = () => {
+    // request next frame
+    requestAnimationFrame(render)
+    // update controls
+    character.update()
+    // render scene and camera
+    renderer.render(scene, camera)
+  }
 
-    // enable character
-    character.enable()
+  // start loop
+  render()
 
-    // render method
-    const render = () => {
-        character.update()
-        requestAnimationFrame(render)
-        renderer.render(scene, camera)
-    }
+  // get app element
+  const element = document.querySelector("#app")
+  // append canvas element
+  element.appendChild(renderer.domElement)
+  // set ready attriute
+  setTimeout(() => element.setAttribute("data-ready", "true"), 1000)
 
-    // remove loader
-    document.querySelector('.loading').remove()
-
-    window.character = character
-
-    // start loop
-    render()
 })
 
 // resize method
 const resize = () => {
-    // get new width
-    const width = window.innerWidth
-    // get new height
-    const height = window.innerHeight
-    // resize renderer
-    renderer.setSize(width, height)
-    // update camera aspect
-    camera.aspect = width / height
-    // update projection matrix
-    camera.updateProjectionMatrix()
+  // get new width
+  const width = window.innerWidth
+  // get new height
+  const height = window.innerHeight
+  // resize renderer
+  renderer.setSize(width, height)
+  // update camera aspect
+  camera.aspect = width / height
+  // update projection matrix
+  camera.updateProjectionMatrix()
 }
 
 // resize on window resize
-window.addEventListener('resize', resize)
+window.addEventListener("resize", resize)
 // resize on window load
-window.addEventListener('load', resize)
+window.addEventListener("load", resize)
